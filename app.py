544e12730c7a810f4ee3b69ede44139b409578d7
swabhisrivastav <swabhi16@gmail.com>
2024-10-10 13:39:42 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,65 @@
+from flask import Flask,render_template,request
+import pickle
+import numpy as np
+
+popular_df = pickle.load(open('popular.pkl','rb'))
+pt = pickle.load(open('pt.pkl','rb'))
+books = pickle.load(open('books.pkl','rb'))
+similarity_scores = pickle.load(open('similarity_scores.pkl','rb'))
+app = Flask(__name__)
+
+@app.route('/')
+
+def index():
+    return render_template('index.html',
+                           book_name = list(popular_df['Book-Title'].values),
+                           author = list(popular_df['Book-Author'].values),
+                           image = list(popular_df['Image-URL-M'].values),
+                           votes = list(popular_df['num_ratings'].values),
+                           rating = list(popular_df['avg_ratings'].values))
+@app.route('/recommend')
+def recommend_ui():
+    return render_template('recommend.html')
+
+@app.route('/recommend_books',methods=['post'])
+def recommend():
+    user_input = request.form.get('user_input')
+    index = np.where(pt.index==user_input)[0][0]
+    similar_items = sorted(list(enumerate(similarity_scores[index])),key=lambda x:x[1],reverse=True)[1:6]
+    
+    data = []
+    for i in similar_items:
+        item = []
+        temp_df = books[books['Book-Title']==pt.index[i[0]]]
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values))
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Author'].values))
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Image-URL-M'].values))
+        
+        data.append(item)
+    
+    print(data)
+
+    return render_template('recommend.html',data=data)
+
+@app.route('/search')
+def search_ui():
+    return render_template('search.html')
+
+@app.route('/byauthor_books',methods=['post'])
+def search_books_by_author():
+    author_name = request.form.get('user_input')
+    # Filter the books by the specified author
+    unique_books = books.drop_duplicates(subset='Book-Title')
+    filtered_books = unique_books[unique_books['Book-Author'].str.contains(author_name, case=False, na=False)]
+    data = []
+    for _, row in filtered_books.iterrows():
+        item = []
+        item.append(row['Book-Title'])
+        item.append(row['Book-Author'])
+        item.append(row['Image-URL-M'])
+        data.append(item)
+
+    return render_template('search.html',data=data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
